89421aa5179e2df1b9c956d1ee55bdb7eea69651
Andres Vargas <zodman@gmail.com>
2019-05-02 19:47:13 -0500

    backup

--- app/app.py
+++ app/app.py
@@ -1,12 +1,21 @@
 from django_micro import configure, route, run
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib import messages
 from django import forms
 from utils import search as search_url
 from utils import votes
 DEBUG = True
 INSTALLED_APPS= [ "widget_tweaks",]
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 configure(locals())
+from django.views.decorators.cache import cache_page
 
 
 class FUrl(forms.Form):
@@ -20,16 +29,18 @@ class FUrl(forms.Form):
 def homepage(request):
     context = dict()
     context["form"] = FUrl()
-    if request.method=="POST":
+    if request.method == "POST":
         f = FUrl(request.POST)
+        context["form"] = f
         if f.is_valid():
-            context["result"] = f.search()
-
-
-    id = request.GET.get("id")
-    if id:
-        result = votes(id)
-        context["votes"] = result
+            id = f.search()
+            if id:
+                total, result = votes(id)
+                context["votes"] = dict(total=total, result=result)
+            else:
+                context["error"]= "Url not found on producthunt.com"
+        else:
+            context["erro"]= "Form invalid"
 
     return render(request, "home.html", context)
 
--- app/utils.py
+++ app/utils.py
@@ -1,34 +1,44 @@
 import requests
-import pprint
+
 h = {
     "Authorization": "Bearer 7112d23e618e967f9475ce6d28a391e50d7ff6924bec1c400ae9a13122d6eede"
 }
 
+def check_maker(username):
+    url = "https://api.getmakerlog.com/users/{}/"
+    r = requests.get(url.format(username))
+    if r.status_code == requests.codes.ok:
+        return True
+    return False
+
+
 def votes(id):
     l = []
     page = 1
+    count = 0
     while True:
-        url ="https://api.producthunt.com/v1/posts/{}/votes?page={}"
-        r =requests.get(url.format(id,page), headers=h)
+        url = "https://api.producthunt.com/v1/posts/{}/votes?page={}"
+        r = requests.get(url.format(id,page), headers=h)
         response = r.json()
-        page +=1
+        page += 1
         if not response.get("votes"):
             break
         for i in response.get("votes"):
             username = i.get("user").get("username")
             image = i.get("user").get("image_url").get("30px")
             name = i.get("user").get("name")
-            l.append(dict(username=username, image=image, name=name))
-    return l
+            is_maker = check_maker(username)
+            if is_maker:
+                l.append(dict(username=username, image=image, name=name, is_maker=is_maker))
+            count += 1
+    return count, l
+
+import pprint
 def search(url_search):
-    url ="https://api.producthunt.com/v1/posts/all?search[url]={}"
-    r =requests.get(url.format(url_search), headers=h)
-    response = r.json()
-    pprint.pprint(response)
-    l = []
-    for post in response.get("posts"):
-        name = post.get("name")
-        id = post.get("id")
-        thumbnail = post.get("thumbnail").get("image_url")
-        l.append(dict(name=name, id=id, thumbnail=thumbnail))
-    return l
+    import re
+    
+    content = requests.get(url_search).text
+    resp = re.search('producthunt\:\/\/post\/(?P<id>[0-9]+)', content)
+    if resp:
+        id =  resp.groupdict().get("id")
+        return id 
